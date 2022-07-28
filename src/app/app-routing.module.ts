f808ae03c0a2af20913b852939f9d2d612735275
Tounsi Asmaa <tasmaa2909@gmail.com>
2022-07-28 21:33:10 +0100

    3rd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmindashComponent } from './admindash/admindash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admindash", component:AdmindashComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
